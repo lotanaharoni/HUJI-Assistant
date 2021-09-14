fbed6682f9d25cf02b2188e3dd07eef5cc480503
lotanaharoni <lotanaharoni@gmail.com>
2021-09-14 12:28:37 +0300

    sidebar

--- app/src/main/java/com/example/huji_assistant/MainActivity.java
+++ app/src/main/java/com/example/huji_assistant/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.huji_assistant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentContainerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout moreInfoDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        moreInfoDrawerLayout = findViewById(R.id.drawer_layout_more_info);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        moreInfoDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        navigationView.setNavigationItemSelectedListener(this);
 
         // Gets fragments
         FragmentContainerView loginFragment = findViewById(R.id.loginfragment);
@@ -24,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
         MainScreen mainScreen = new MainScreen();
         getSupportFragmentManager().beginTransaction().replace(loginFragment.getId(), firstFragment)
                 .commit();
+
+        findViewById(R.id.buttonMoreInfoMapActivity).setOnClickListener(v -> {
+            moreInfoDrawerLayout.openDrawer(GravityCompat.START);
+        });
 
         firstFragment.newUserBtnListener = new FirstFragment.newUserButtonClickListener() {
             @Override
@@ -90,5 +109,42 @@ public class MainActivity extends AppCompatActivity {
                         .replace(loginFragment.getId(), mainScreen).disallowAddToBackStack().commit();
             }
         };
+    }
+
+    private void goToUrl(String s) {
+        Uri url = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW, url));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.aguda:
+                goToUrl("https://www.aguda.org.il/");
+                break;
+            case R.id.kolzchut:
+                goToUrl("https://www.kolzchut.org.il/he/%D7%A1%D7%98%D7%95%D7%93%D7%A0%D7%98%D7%99%D7%9D");
+                break;
+            case R.id.link_to_drive:
+                goToUrl("https://drive.google.com/drive/u/3/folders/0B4NFaiXelmmkelRNeFpRMHlVX2M?resourcekey=0-5b6GE4omL97bd0wCXvzv4w");
+                break;
+            case R.id.rishumnet:
+                goToUrl("https://rishum-net.huji.ac.il/site/student/login.asp");
+                break;
+            case R.id.recommendations:
+                goToUrl("https://docs.google.com/spreadsheets/d/1ZeeEIkyVsfxV2R7kS_1Jcq06Tu1zu20Y3XT1q9cybtk/edit#gid=0");
+                break;
+        }
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (moreInfoDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            moreInfoDrawerLayout.closeDrawer(GravityCompat.START);
+            return;
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
