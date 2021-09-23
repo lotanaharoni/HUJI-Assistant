a93a50a9a31656a1924be90c0312911eca975e3a
liorait <liorali22@gmail.com>
2021-09-23 10:18:15 +0300

    screensedit

--- app/src/main/java/com/example/huji_assistant/CourseInfoFragment.java
+++ app/src/main/java/com/example/huji_assistant/CourseInfoFragment.java
@@ -2,6 +2,7 @@ package com.example.huji_assistant;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +19,8 @@ public class CourseInfoFragment extends Fragment {
     String courseName;
     String courseNumber;
     String coursePoints;
+    TextView courseNameTextView;
+    TextView courseNumberTextView;
 
     public CourseInfoFragment(){
         super(R.layout.courseinfo_fragment);
@@ -27,12 +30,16 @@ public class CourseInfoFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         viewModelAppMainScreen = new ViewModelProvider(requireActivity()).get(ViewModelAppMainScreen.class);
+        courseNameTextView = view.findViewById(R.id.courseInfoName);
+        courseNumberTextView = view.findViewById(R.id.courseInfoNumber);
 
         viewModelAppMainScreen.get().observe(getViewLifecycleOwner(), item -> {
             courseName = item.getName();
             courseNumber = item.getId();
          //   coursePoints = item.getPoints();
-            System.out.println("name: " + courseName + " number: " + courseNumber);
+           // System.out.println("name: " + courseName + " number: " + courseNumber);
+            courseNameTextView.setText(courseName);
+            courseNumberTextView.setText(courseNumber);
         });
         // todo send here the item to display
     }
