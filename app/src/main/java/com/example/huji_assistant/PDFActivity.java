e13f943ed5dcd9b2353f79b8d3467837ebb9d8b5
lotanaharoni <lotanaharoni@gmail.com>
2021-09-23 18:08:32 +0300

    Fix pdf

--- app/src/main/java/com/example/huji_assistant/PDFActivity.java
+++ app/src/main/java/com/example/huji_assistant/PDFActivity.java
@@ -1,8 +1,12 @@
 package com.example.huji_assistant;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,11 +16,18 @@ import java.net.URLEncoder;
 public class PDFActivity extends AppCompatActivity {
 
     WebView webView;
+    Activity activity ;
+    private ProgressDialog progDailog;
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
+        activity = this;
+
+        progDailog = ProgressDialog.show(activity, "Loading","Please wait...", true);
+        progDailog.setCancelable(false);
 
         webView = findViewById(R.id.webview);
 
@@ -24,6 +35,23 @@ public class PDFActivity extends AppCompatActivity {
         String path=i.getExtras().getString("PATH");
 
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                progDailog.show();
+                view.loadUrl(url);
+
+                return true;
+            }
+            @Override
+            public void onPageFinished(WebView view, final String url) {
+                progDailog.dismiss();
+            }
+        });
+
         String url = "";
         try {
             url= URLEncoder.encode(path,"UTF-8");
@@ -31,5 +59,8 @@ public class PDFActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + url);
+
+//        webView.loadUrl("http://www.teluguoneradio.com/rssHostDescr.php?hostId=147");
+
     }
 }
