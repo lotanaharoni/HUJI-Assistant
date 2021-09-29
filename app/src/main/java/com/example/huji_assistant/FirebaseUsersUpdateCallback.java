dd02fcb4ff5aad50dbe45802e896119fca5bf9db
lotanaharoni <lotanaharoni@gmail.com>
2021-09-29 16:35:40 +0300

    remove FirebaseUsersUpdateCallback.java

--- app/src/main/java/com/example/huji_assistant/FirebaseUsersUpdateCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.huji_assistant;
-
-import java.util.HashMap;
-
-import com.example.huji_assistant.StudentInfo;
-
-public interface FirebaseUsersUpdateCallback {
-    void onCallback(HashMap<String, StudentInfo> users);
-}
